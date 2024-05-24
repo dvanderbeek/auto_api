3d2072c681aa275a918db21da21791da0a716c92
David Van Der Beek <earlynovrock@gmail.com>
2024-05-23 20:39:33 -0700

    add delete mutation

--- app/graphql/mutations/dynamic_mutation.rb
+++ app/graphql/mutations/dynamic_mutation.rb
@@ -5,20 +5,18 @@ module Mutations
         graphql_name "#{model.name.gsub('::', '')}#{action.capitalize}"
         types = Types::Objects::BaseModelType.register(model)
 
-        argument :id, type: GraphQL::Types::BigInt if action == 'update'
-        argument :input, types[:input_type], required: true
-        # input_object_class types[:input_type]
+        argument :id, type: GraphQL::Types::BigInt if %w[update delete].include?(action)
+        argument :input, types[:input_type], required: true unless action == 'delete'
 
         field :result, types[:type], null: false
 
         define_method :resolve do |args|
-          resource = action == 'create' ? model.new(**args[:input]) : model.find(args[:id])
-          resource.assign_attributes(**args[:input]) unless action == 'create'
-          if resource.save
-            { result: resource }
-          else
-            raise GraphQL::ExecutionError.new(resource.errors.full_messages.join(', '))
-          end
+          resource = action == 'create' ? model.new : model.find(args[:id])
+          resource.assign_attributes(**args[:input]) unless action == 'delete'
+
+          raise GraphQL::ExecutionError.new(resource.errors.full_messages.join(', ')) unless (action == 'delete' ? resource.destroy : resource.save)
+
+          { result: resource }
         end
       end
     end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,6 +5,7 @@ module Types
     ApplicationRecord.descendants.each do |model|
       field :"#{model.name.gsub('::', '').underscore}_create", mutation: Mutations::DynamicMutation.create_mutation(model, 'create')
       field :"#{model.name.gsub('::', '').underscore}_update", mutation: Mutations::DynamicMutation.create_mutation(model, 'update')
+      field :"#{model.name.gsub('::', '').underscore}_delete", mutation: Mutations::DynamicMutation.create_mutation(model, 'delete')
     end
   end
 end
