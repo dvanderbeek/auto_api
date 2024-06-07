2e99d76635bf622f6eb1f850f6d0f04d4e122884
David Van Der Beek <earlynovrock@gmail.com>
2024-06-07 10:06:01 -0700

    add note about using examples for attr type instead of defining example hashes

--- app/models/blog/post/attrs.rb
+++ app/models/blog/post/attrs.rb
@@ -1,6 +1,9 @@
 module Blog
   class Post
     class Attrs
+      # TODO: Create example values for various attribute types and auto-generate these examples for the docs
+      # For example, mark an attribute as an EthereumAddress (via a custom ActiveModel::Type), we could set it up to automatically
+      # use Faker::Blockchain::Ethereum.address (and so forth). Then example values would be re-usable across resources
       def self.valid
         {
           id: 1,
