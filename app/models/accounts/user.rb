b6cf3c37384fe85c91697808d15b1e576613f45b
David Van Der Beek <earlynovrock@gmail.com>
2024-08-26 13:35:50 -0700

    use streamlined lambda for autopopulate instead of model method

--- app/models/accounts/user.rb
+++ app/models/accounts/user.rb
@@ -3,13 +3,13 @@ module Accounts
     attribute :validated, :boolean, default: true
     validates :name, name: true
 
-    autopopulates :name, with: :fetch_name
+    # autopopulates :name, with: :fetch_name
     # OR a proc / lambda / other callable object
-    # autopopulates :name, with: ->(user) { NameApiClient.new.get_name(user) }
+    autopopulates :name, with: ->(user) { NameApiClient.new.get_name(user) }
     # autopopulates :name, with: NameApiClient.new.method(:get_name)
 
-    def fetch_name
-      NameApiClient.new.get_name(self)
-    end
+    # def fetch_name
+    #   NameApiClient.new.get_name(self)
+    # end
   end
 end
