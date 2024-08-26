4d1a43bf0c08652621bf8d982413463aec6c9798
David Van Der Beek <earlynovrock@gmail.com>
2024-08-26 13:26:49 -0700

    move api call into api client service

--- app/models/accounts/user.rb
+++ app/models/accounts/user.rb
@@ -1,16 +1,15 @@
 module Accounts
   class User < ApplicationRecord
-    include Autopopulateable
-
     attribute :validated, :boolean, default: true
     validates :name, name: true
 
-    # autopopulates :name, with: ->(user) { puts 'Fetching name from remote API'; 'david' }
-    autopopulates :name, with: :fetch_my_name
+    autopopulates :name, with: :fetch_name
+    # OR a proc / lambda / other callable object
+    # autopopulates :name, with: ->(user) { NameApiClient.new.get_name(user) }
+    # autopopulates :name, with: NameApiClient.new.method(:get_name)
 
-    def fetch_my_name
-      puts 'Fetching value from remote API'
-      'DVDB'
+    def fetch_name
+      NameApiClient.new.get_name(self)
     end
   end
 end
---
+++ app/services/name_api_client.rb
@@ -0,0 +1,6 @@
+class NameApiClient
+  def get_name(_user)
+    puts 'Fetching name from remote API'
+    'DVDB'
+  end
+end
